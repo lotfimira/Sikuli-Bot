0bb33a9ff6a87aa6f2de239d27a0640e9b9f33eb
MIRAGEOSCIENCE\lotfim <lotfim@mirageoscience.com>
2017-07-28 19:19:56 -0400

    Fetch test result from log file

--- sikulibot.py
+++ sikulibot.py
@@ -10,7 +10,8 @@ from shutil import copyfile
 INSTALLERS_PATH = 'Y:\Shared\Scratch\_Autobuild\Insight'
 TEST_LOG_PATH = 'E:\SikuliTestLogs'
 SIKULI_TESTS_FOLDER = 'TestSikuli'
-WORKSPACE_PATH = 'E:\workspace'
+WORKSPACE_PATH = 'E:\\workspace'
+SIKULI_TESTS_PATH = WORKSPACE_PATH + '\\' + SIKULI_TESTS_FOLDER
 SSH_HOME = 'E:/application/github' # location of git hub ssh key (no passphrase)
 TODAY_00H00 = dt.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 THIS_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -126,7 +127,7 @@ def getBranchName(installer_name):
     return branch_name
 
 #------------------------------------------------------------------------------
-def getRemoteNameAndSha(branch_name):
+def getRemoteName(branch_name):
     
     personal_access_token = open(THIS_FILE_PATH + '/token.txt').read()   
     g = Github(personal_access_token)
@@ -135,9 +136,10 @@ def getRemoteNameAndSha(branch_name):
     for pr in pull_requests:
         head = pr.head
         if branch_name in head.label:
-            return head.repo.full_name, head.sha # repo name + commit id
-            
-    return 'MiraGeoscience/InSight', 'development'
+            return head.repo.full_name
+
+    print('FAILED TO GET REMOTE NAME')
+    return 'MiraGeoscience/InSight'
 
 #------------------------------------------------------------------------------
 def remove_readonly(func, path, excinfo):
@@ -145,34 +147,37 @@ def remove_readonly(func, path, excinfo):
     func(path)
 
 #------------------------------------------------------------------------------
-def getSources(remote_name, sha, dest_path):
-    
+def getSources(remote_name, branch_name, dest_path):
+
+    print("REMOTE NAME " + remote_name);
+    print("BRANCH NAME " + branch_name);
+
     # clean up workspace
     if os.path.isdir(dest_path):
         shutil.rmtree(dest_path, onerror=remove_readonly)
 
     os.makedirs(dest_path)
-    
+
     # set location of ssh key (no passphrase)
     os.environ['HOME'] = SSH_HOME
-        
+
     # clone repo
     clone_url = 'git@github.com:' + remote_name
     res = subprocess.call(["git", "clone", clone_url, dest_path])
     if res is not 0:
         print('Failed to clone ' + remote_name)
         exit(res)
-    
+
     # checkout branch
     res = subprocess.call(["git", \
                            "--git-dir=" + dest_path + "/.git", \
                            "--work-tree=" + dest_path, \
                            "checkout", \
-                           sha, \
+                           branch_name, \
                            "--quiet"])
 
     if res is not 0:
-        print('Failed to checkout branch ' + remote_name + ':' + remote_name)
+        print('Failed to checkout branch ' + remote_name + ':' + branch_name)
         exit(res)
 
 #------------------------------------------------------------------------------
@@ -181,17 +186,36 @@ def touch(path):
     with open(path, 'a'):
         # update mtime in case it exists
         os.utime(path, None)
-        
-#------------------------------------------------------------------------------
-def saveLog(path, installer_name):
 
-    log_file = path + '/log.txt'
-    saved_log_file = TEST_LOG_PATH + '/' + getBuildName(installer_name) + '.txt'
-    
+#------------------------------------------------------------------------------
+def savedLogFile(installer_name):
+    return TEST_LOG_PATH + '/' + getBuildName(installer_name) + '.txt'
+
+#------------------------------------------------------------------------------
+def saveLog(target_file):
+
+    log_file = SIKULI_TESTS_PATH + '/log.txt'
+
     if os.path.exists(log_file):
-        copyfile(log_file, saved_log_file)
+        copyfile(log_file, target_file)
     else:
-        touch(saved_log_file)
+        touch(target_file)
+
+#------------------------------------------------------------------------------
+def getFailedTestsFromLogFile(log_file):
+
+    content = []
+    with open(log_file) as file:
+        content = file.readlines()
+
+    fail_list = []
+    for line in content:
+        if line.upper().startswith('FAIL'):
+            test_file_path = (line.split())[-1]
+            test_file_path = test_file_path.replace(SIKULI_TESTS_PATH + '/Test\\', "")
+            fail_list.append(test_file_path)
+
+    return fail_list
 
 #------------------------------------------------------------------------------
 def sendZulipMessage(msg):
@@ -212,11 +236,11 @@ if len(installer_to_test) == 0:
 
 # identify repository and branch
 branch_name = getBranchName(installer_to_test)
-remote_name, sha = getRemoteNameAndSha(branch_name)
+remote_name = getRemoteName(branch_name)
 
 # get source code
 print('Checking out ' + remote_name + ':' + branch_name + '...')
-getSources(remote_name, sha, WORKSPACE_PATH)
+getSources(remote_name, branch_name, WORKSPACE_PATH)
     
 # install Analyst via setup.exe
 print('Installing ' + installer_to_test + '...')
@@ -231,27 +255,31 @@ else:
 
 # run sikuli tests
 os.environ['ANALYST_PATH'] = 'C:/Program Files/Mira Geoscience/Geoscience ANALYST'
-sikuli_tests_path = WORKSPACE_PATH + '/' + SIKULI_TESTS_FOLDER
 
-if not os.path.isdir(sikuli_tests_path):
+if not os.path.isdir(SIKULI_TESTS_PATH):
     print('No Sikuli tests found on this branch')
-    saveLog(sikuli_tests_path, installer_to_test)
+    saveLog(SIKULI_TESTS_PATH, installer_to_test)
     exit(0)
 
-sys.path.append(sikuli_tests_path)
-os.system(sikuli_tests_path + '/RunAllTests.py installed')
+sys.path.append(SIKULI_TESTS_PATH)
+os.system(SIKULI_TESTS_PATH + '/RunAllTests.py installed')
 
 # save a copy of sikuli test log, so we know what build we tested
-saveLog(sikuli_tests_path, installer_to_test)
+target_log = savedLogFile(installer_to_test)
+saveLog(target_log)
+
+# list failed tests
+fail_list = getFailedTestsFromLogFile(target_log)
 
 # write a message to zulip about test result
-if os.environ.get('TEST_SUCCESS') == 'TRUE':
+if len(fail_list) == 0:
     sendZulipMessage(installer_to_test + "\nSuccess :heavy_check_mark:")
 else:
     message = installer_to_test + "\nFAILURE :x:"
-    fail_list = os.environ.get('FAIL_LIST').split(';')
-    
+
     for filename in fail_list:
-        message += '\n' + filename
-        
+        message += '\n- ' + filename
+
     sendZulipMessage(message)
+
+
